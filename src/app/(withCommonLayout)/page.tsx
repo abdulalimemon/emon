a438d77eeae10e224d93110fc411db1ac3d499c6
emon02 <abdulalimemon11@gmail.com>
2024-04-26 17:29:01 +0600

    added comming soon page

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,9 @@
+import CommingSoon from "@/components/pages/commingSoon";
+
+export default function Home() {
+  return (
+    <main>
+      <CommingSoon />
+    </main>
+  );
+}
---
+++ src/components/pages/commingSoon/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const CommingSoon = () => {
+  return (
+    <section className="py-10 bg-slate-500">
+      <h2>Welcome to the Comming Soon page</h2>
+      <Button>Hello</Button>
+    </section>
+  );
+};
+
+export default CommingSoon;
