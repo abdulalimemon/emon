93224fbb45b60aaaabf21fe33c0faff7431e2c05
emon02 <abdulalimemon11@gmail.com>
2024-05-05 22:09:13 +0600

    added resume download option

--- src/components/pages/home/HeroSection.tsx
+++ src/components/pages/home/HeroSection.tsx
@@ -32,18 +32,22 @@ const HeroSection = () => {
               </div>
               <div className="flex flex-col lg:flex-row gap-5 items-center justify-center">
                 <Link href="/contact">
-                  <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                  <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 focus:ring-offset-slate-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-100 dark:bg-slate-900 px-20 py-5 text-sm font-medium text-black hover:bg-slate-200 hover:dark:bg-black dark:text-white backdrop-blur-3xl transition duration-500">
-                    Get In Touch
+                      Get In Touch
                     </span>
                   </button>
                 </Link>
-                <Link href="/contact">
-                  <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                <Link
+                  href="/public/Abdul Alim Emon Resume.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 focus:ring-offset-slate-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-100 dark:bg-slate-900 px-20 py-5 text-sm font-medium text-black hover:bg-slate-200 hover:dark:bg-black dark:text-white backdrop-blur-3xl transition duration-500">
-                    <IoMdCloudDownload className="size-5 mr-3" />
+                      <IoMdCloudDownload className="size-5 mr-3" />
                       <span>Resume</span>
                     </span>
                   </button>
