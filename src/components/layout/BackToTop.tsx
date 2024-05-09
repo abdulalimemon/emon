ed5a5d7a088975286bad2081a85aa0082fa7e25a
emon02 <abdulalimemon11@gmail.com>
2024-05-09 22:14:45 +0600

    added some changes

--- src/components/layout/BackToTop.tsx
+++ src/components/layout/BackToTop.tsx
@@ -1,45 +1,47 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
-import { BiUpArrowAlt } from 'react-icons/bi';
+import { useEffect, useState } from "react";
+import { BiUpArrowAlt } from "react-icons/bi";
 
 const BackToTop = () => {
-    const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const toggleVisibility = () => {
-        if (window.pageYOffset > 300) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
     };
+  }, []);
 
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', toggleVisibility);
-
-        return () => {
-            window.removeEventListener('scroll', toggleVisibility);
-        };
-    }, []);
-
-    return (
-        <div className='fixed bottom-4 right-4 z-50'>
-            <button
-                type='button'
-                onClick={scrollToTop}
-                className={`inline-flex items-center p-2 md:p-3 rounded-full shadow-lg text-black bg-color-yellow hover:opacity-80 transition-opacity overflow-y-auto ${isVisible ? 'flex' : 'hidden'}`}
-            >
-                <BiUpArrowAlt className='size-6 text-black' aria-hidden='true' />
-            </button>
-        </div>
-    );
+  return (
+    <div className="fixed bottom-4 right-4 z-50">
+      <button
+        type="button"
+        onClick={scrollToTop}
+        className={`inline-flex items-center p-2 md:p-3 rounded-full shadow-lg text-black bg-color-yellow hover:opacity-80 transition-opacity overflow-y-auto ${
+          isVisible ? "flex" : "hidden"
+        }`}
+      >
+        <BiUpArrowAlt className="size-6 text-black" aria-hidden="true" />
+      </button>
+    </div>
+  );
 };
 
 export default BackToTop;
