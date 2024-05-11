cada7d5e8581c570fafa77901e67239483410ce4
emon02 <abdulalimemon11@gmail.com>
2024-05-11 22:54:35 +0600

    fixed some code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,34 +13,36 @@ import { Label } from "@/components/ui/label";
 
 const LoginPage = () => {
   return (
-    <section>
+    <section className="py-20">
       <Container>
-        <Card className="w-full max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-2xl">Login</CardTitle>
-            <CardDescription>
-              Enter your email below to login to your account.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full">Sign in</Button>
-          </CardFooter>
-        </Card>
+        <form className="flex items-center justify-center">
+          <Card className="w-full max-w-sm mx-auto">
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button type="submit" className="w-full">Log in</Button>
+            </CardFooter>
+          </Card>
+        </form>
       </Container>
     </section>
   );
