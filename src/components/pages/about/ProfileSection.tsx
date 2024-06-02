ae2cef6f3e55f20585095916037a20581e3e42b6
emon02 <abdulalimemon11@gmail.com>
2024-06-02 16:42:26 +0600

    added profile picture

--- src/components/pages/about/ProfileSection.tsx
+++ src/components/pages/about/ProfileSection.tsx
@@ -1,14 +1,21 @@
 import Container from "@/components/layout/Container";
-import picture from "/public/Abdul Alim Emon Cover Picture.png";
+// import picture from "/Abdul Alim Emon Cover Picture.png";
 import Image from "next/image";
 
 const ProfileSection = () => {
   return (
     <section>
+      <div className="w-full lg:hidden">
+        <Image width={1200} height={400} src="/Abdul Alim Emon Cover Picture.png" className="w-full" alt="Cover Picture" />
+      </div>
       <Container>
-        <div className="rounded-xl">
-          <Image width={1200} height={400} src={picture} alt="Cover Picture" />
-        </div>
+      <div className="hidden lg:block">
+        <Image width={1200} height={200} src="/Abdul Alim Emon Cover Picture.png" className="rounded-b-lg h-[280px]" alt="Cover Picture" />
+      </div>
+      <div className="mt-[-40px] lg:mt-[-60px]">
+        <Image width={320} height={320} src="/Abdul Alim Emon.png" className="w-[80px] lg:w-[120px] mx-auto rounded-full" alt="Profile Picture" />
+      </div>
+      div
       </Container>
     </section>
   );
