83e1f4b5ab24a5530a882d43046acbaa5e75e4ca
emon02 <abdulalimemon11@gmail.com>
2024-06-09 16:37:39 +0600

    complete portfolio loading page

--- src/app/(withCommonLayout)/portfolio/loading.tsx
+++ src/app/(withCommonLayout)/portfolio/loading.tsx
@@ -6,27 +6,53 @@ const Loading = () => {
     <section className="py-20 bg-slate-100 dark:bg-black">
       <Container>
         <div>
-          <h2 className="text-center text-2xl lg:text-4xl font-semibold py-5 lg:py-10">
-            Projects.
-          </h2>
-          <Skeleton />
+          <Skeleton className="h-14 w-1/2 mx-auto mb-10 bg-slate-800" />
         </div>
 
         <div className="grid grid-cols-1 gap-10">
           <div className="flex flex-col-reverse lg:flex-row p-5 lg:py-10 lg:px-12 shadow-lg w-full gap-5 rounded-[22px] bg-gray-50 dark:bg-slate-900">
             <div className="w-full lg:w-6/12 flex flex-col justify-evenly">
+              <Skeleton className="h-10 w-1/2 mb-5 bg-slate-800 " />
               <h2 className="text-xl lg:text-2xl font-bold py-3"></h2>
-              <p className="text-base lg:text-2xl pb-3"></p>
-              <div className="inline-block">
-                <span className="inline-block">
-                  <Skeleton className="py-3 flex items-center justify-start hover:text-color-yellow dark:hover:text-link">
-                    <span>View details </span>{" "}
-                  </Skeleton>
-                </span>
+              <Skeleton className="h-5 mb-5 bg-slate-800 " />
+              <Skeleton className="h-5 mb-5 bg-slate-800 " />
+              <Skeleton className="h-5 bg-slate-800 " />
+              <div className="inline-block">              
+                <Skeleton className="h-10 w-1/2 my-10 bg-slate-800"/>                   
               </div>
             </div>
-            <div className="w-full lg:w-6/12">
-              <Skeleton className="w-full" />
+            <div className="w-full lg:w-6/12 h-full flex items-center">
+              <Skeleton className="h-60 w-full" />
+            </div>
+          </div>
+          <div className="flex flex-col-reverse lg:flex-row p-5 lg:py-10 lg:px-12 shadow-lg w-full gap-5 rounded-[22px] bg-gray-50 dark:bg-slate-900">
+            <div className="w-full lg:w-6/12 flex flex-col justify-evenly">
+              <Skeleton className="h-10 w-1/2 mb-5 bg-slate-800 " />
+              <h2 className="text-xl lg:text-2xl font-bold py-3"></h2>
+              <Skeleton className="h-5 mb-5 bg-slate-800 " />
+              <Skeleton className="h-5 mb-5 bg-slate-800 " />
+              <Skeleton className="h-5 bg-slate-800 " />
+              <div className="inline-block">              
+                <Skeleton className="h-10 w-1/2 my-10 bg-slate-800"/>                   
+              </div>
+            </div>
+            <div className="w-full lg:w-6/12 h-full flex items-center">
+              <Skeleton className="h-60 w-full" />
+            </div>
+          </div>
+          <div className="flex flex-col-reverse lg:flex-row p-5 lg:py-10 lg:px-12 shadow-lg w-full gap-5 rounded-[22px] bg-gray-50 dark:bg-slate-900">
+            <div className="w-full lg:w-6/12 flex flex-col justify-evenly">
+              <Skeleton className="h-10 w-1/2 mb-5 bg-slate-800 " />
+              <h2 className="text-xl lg:text-2xl font-bold py-3"></h2>
+              <Skeleton className="h-5 mb-5 bg-slate-800 " />
+              <Skeleton className="h-5 mb-5 bg-slate-800 " />
+              <Skeleton className="h-5 bg-slate-800 " />
+              <div className="inline-block">              
+                <Skeleton className="h-10 w-1/2 my-10 bg-slate-800"/>                   
+              </div>
+            </div>
+            <div className="w-full lg:w-6/12 h-full flex items-center">
+              <Skeleton className="h-60 w-full" />
             </div>
           </div>
         </div>
