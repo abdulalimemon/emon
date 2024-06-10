5852c9a3187932b7f5f539b677f62fd88d9c3e3d
emon02 <abdulalimemon11@gmail.com>
2024-06-10 16:56:15 +0600

    added skill form

---
+++ src/components/pages/Dashboard/skills/AddSkillForm.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+const AddSkillForm = () => {
+  return (
+    <div className="flex py-10 items-center justify-center rounded-lg border border-dashed shadow-sm">
+      <div className="flex items-center justify-between mb-10">
+        <h3 className="text-xl font-semibold tracking-tight">Add Skill</h3>
+        <Link href="/dashboard/skills/add">
+          <Button className="dark:text-black">
+            <FaExternalLinkAlt className="size-5 mr-3" /> View All Skill
+          </Button>
+        </Link>
+      </div>
+
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Product Details</CardTitle>
+            <CardDescription>
+              Lipsum dolor sit amet, consectetur adipiscing elit
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  className="w-full"
+                  defaultValue="Gamer Gear Pro Controller"
+                />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="description">Description</Label>
+                <Textarea
+                  id="description"
+                  defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
+                  className="min-h-32"
+                />
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default AddSkillForm;
